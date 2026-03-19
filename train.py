cc7637f5a227248ddc9ed913d4629f350051dd01
pnaclcu <546312172@qq.com>
2026-03-19 10:28:06 +0800

    add links for training models

--- train.py
+++ train.py
@@ -1,23 +1,25 @@
 from ultralytics import YOLO
-from swanlab.integration.ultralytics import add_swanlab_callback
+
 
 model = YOLO('yolov8m-fra.yaml')
 
-# add_swanlab_callback(
-#     model,
-#     project="ultralytics",
-#     experiment_name="FRA-YOLO",
-#     description="yolov8m on visdrone",
-#
-#
-# )
+using_swanlab = False
+if using_swanlab:
+    from swanlab.integration.ultralytics import add_swanlab_callback
+    add_swanlab_callback(
+        model,
+        project="ultralytics",
+        experiment_name="FRA-YOLO",
+        description="yolov8m on visdrone",
+    )
+
 train_results = model.train(
-    data="visdrone10.yaml",  # Path to dataset configuration file
+    data="VisDrone.yaml",  # Path to dataset configuration file, the VisDrone will be downloaded automaticall.
     epochs=350,  # Number of training epochs
     imgsz=640,  # Image size for training
     device='0',
     plots=True,
-    batch = 4,
+    batch = 8,
     amp = False,
 )
 metrics = model.val()
